370449ee1f1b4452a18e76230ae668da7855770c
Zee Spencer <zee@zeespencer.com>
2013-10-08 12:02:09 -0700

    WIP - Test newly registered users are thanked

--- spec/features/guest_may_register_spec.rb
+++ spec/features/guest_may_register_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
 
 feature "Guest may register as Norse Deity" do
-  scenario "Guest is thanked after registering"
+  scenario "Guest is thanked after registering" do
+    visit "/"
+    click_on "Register as a Norse Deity"
+
+    fill_in "user_name", with: "Zee"
+    fill_in "user_email", with: "zee@example.com"
+    fill_in "user_password", with: "password"
+
+    click_on "Finish Registering"
+
+    expect(page).to have_content("Thanks for registering, Zee!")
+  end
+
   scenario "Guest is logged in immediately after registering"
   scenario "Guest must provide an email address"
   scenario "Guest must provide a password"
