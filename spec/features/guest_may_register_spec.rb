49479ef65eadc2fc86737443c040a8a0bbdf2d16
Zee Spencer <zee@zeespencer.com>
2013-10-08 11:59:50 -0700

    Outline Guest May Register as Norse Deity feature

---
+++ spec/features/guest_may_register_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature "Guest may register as Norse Deity" do
+  scenario "Guest is thanked after registering"
+  scenario "Guest is logged in immediately after registering"
+  scenario "Guest must provide an email address"
+  scenario "Guest must provide a password"
+end
