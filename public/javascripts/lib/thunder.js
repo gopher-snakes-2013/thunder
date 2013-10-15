2e6a95bef77c8669fbf35b1580494927cfbda89c
Zee Spencer <zee@zeespencer.com>
2013-10-14 20:34:23 -0700

    Add JS and specs for showing the add comment form

--- public/javascripts/lib/thunder.js
+++ public/javascripts/lib/thunder.js
@@ -1,5 +1,10 @@
 Thunder = {
   run: function() {
+    $('.add-note-form').addClass('hidden');
 
+    $('.add-note-link').on('click', function() {
+      $('.add-note-form').removeClass('hidden');
+      $(this).addClass('hidden');
+    });
   }
 }
--- spec/javascripts/thunderSpec.js
+++ spec/javascripts/thunderSpec.js
@@ -1,8 +1,35 @@
 describe('Thunder', function () {
   describe('.run', function() {
-    it('should be defined', function() {
-      expect(Thunder.run).toBeDefined();
+    beforeEach(function() {
+      var talkDOM = affix('.talk');
+      talkDOM.affix('.add-note-link');
+      talkDOM.affix('.add-note-form')
+
+      // Think of the DOM like a database. We're inserting HTML into it so we
+      // can actually test that it behaves properly given the  structure of the
+      // data
+
+      // That's what affix does for us. It inserts HTML into the DOM which it
+      // reverse engineers from CSS Selectors.
+
+
+      Thunder.run();
+    });
+
+    it("hides the add-note-form", function() {
+      expect($('.add-note-form')).toHaveClass('hidden');
     })
+
+    it("shows the add-note-form when the add-note-link is clicked", function() {
+      $('.add-note-link').click();
+
+      expect($('.add-note-form')).not.toHaveClass('hidden');
+    });
+    it("hides the add-note-link when it is clicked", function() {
+      $('.add-note-link').click();
+
+      expect($('.add-note-link')).toHaveClass('hidden');
+    });
   });
 });
 
