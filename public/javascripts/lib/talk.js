1053e2dcde3e4a17f337c675b784d7b42316def6
Zee Spencer <zee@zeespencer.com>
2013-10-17 12:23:24 -0700

    Talks with ids are saved via a PUT to /talks/:id
    Here I'm agressively extracting private methods, signified by the `__` or
    'dunderscore'. I'm choosing not to test these private methods because they're
    incidental to the behavior of the Talk object.
    
    I'm choosing to expose `isPersisted`, because I can imagine it being useful to
    outside collaborators. Some people will wait until another object actually
    wants to call the isPersistd method befor publicly exposing it. This is also
    valid.
    
    I also refactored my test organization to focus more on the `inputs` (i.e. what
    attributes the talk is instantiated with) than the individual methods. This is
    somewhat controversial; as standard practice is to describe each method with
    the permutations next to it.
    
    Some of you may be thinking "man, it looks like the Talk object does too many
    things. It's sending stuff to the server as well as holding the talk
    attributes!
    
    To you I say, well done. I concur. However, because there isn't a need to share
    this behavior quite yet I will wait to extract an object which is responsible
    for sending data to the server.

--- public/javascripts/lib/talk.js
+++ public/javascripts/lib/talk.js
@@ -4,9 +4,31 @@ function Talk(attributes) {
 
 Talk.prototype.save = function() {
   $.ajax({
-    url: '/talks',
-    method: "post",
+    url: this.__url(),
+    method: this.__method(),
     contentType: "json",
     data: JSON.stringify(this.attributes)
   });
+}
+
+Talk.prototype.__url = function() {
+// The double underscores indicate that this function is private.
+  if (this.isPersisted()) {
+    return  "/talks/" + this.attributes.id
+  } else {
+    return "/talks"
+  }
+}
+
+Talk.prototype.__method = function() {
+
+  if (this.isPersisted()) {
+    return "PUT"
+  } else {
+    return "POST"
+  }
+}
+
+Talk.prototype.isPersisted = function() {
+  return (typeof this.attributes.id !== "undefined")
 }
--- spec/javascripts/talk-model-spec.js
+++ spec/javascripts/talk-model-spec.js
@@ -1,48 +1,82 @@
 describe('Talk', function () {
   var xhr, lastRequest;
-  describe('#save', function() {
-    beforeEach(function() {
-      xhr = sinon.useFakeXMLHttpRequest();
-      // Sinon allows us to intercept all AJAX calls, regardless of whether
-      // we're using jQuery or another library.
+  beforeEach(function() {
+    xhr = sinon.useFakeXMLHttpRequest();
+    // Sinon allows us to intercept all AJAX calls, regardless of whether
+    // we're using jQuery or another library.
 
-      xhr.onCreate = function(request) {
+    xhr.onCreate = function(request) {
       // by setting  onCreate to a function, we make it so every time a new
       // AJAX call is created we can capture it and store it somewhere.
-        lastRequest = request;
-        // In this case, we're storing it as 'lastRequest'
-      };
+      lastRequest = request;
+      // In this case, we're storing it as 'lastRequest'
+    };
+  });
+
+  afterEach(function() {
+    xhr.restore();
+    // We want to turn AJAX back on. Just in case.
+  });
+
+  context("When the talk is new", function() {
+    var talk;
+    beforeEach(function() {
+      talk = new Talk({name: "Do the dance" });
+      talk.save();
     });
 
-    afterEach(function() {
-      xhr.restore();
-      // We want to turn AJAX back on. Just in case.
-    });
-
-    it("sends the talks data in the request body", function() {
-        var talk = new Talk({id: 5, name: "Do the dance" });
-
-        talk.save();
-
-        expect(lastRequest.requestBody).toEqual(JSON.stringify(talk.attributes));
-    });
-    it("returns a deferred object");
-    context("When the talk is new", function() {
-      it("POSTs a request to /talks", function() {
-        var talk = new Talk({name: "Do the dance" });
-        // Let's create a new talk to save. We're not going to use an id
-
-        talk.save();
-        // save is our method under test, so let's call it!
-
+    describe("#save", function() {
+      it("sends the request to /talks", function() {
         expect(lastRequest.url).toEqual('/talks');
-        // Let's make sure the request was sent to the right place!
+      });
+
+      it("sends the request as a POST method", function() {
+        expect(lastRequest.method).toEqual('POST');
       });
     });
 
-    context("When the talks already has an id", function() {
-      xit("PUTs the request to /talks/:talk_id");
+    describe('#isPersisted', function() {
+      it("is false", function() {
+        expect(talk.isPersisted()).toBeFalsy();
+      });
     });
+  });
+
+  context("When the talks already has an id", function() {
+    var talk;
+    beforeEach(function() {
+      talk = new Talk({id: 1, name: "Do the dance" });
+      talk.save();
+    });
+
+    describe('#save', function() {
+      it("sends the request to /talks/:talk_id", function() {
+        expect(lastRequest.url).toEqual('/talks/1');
+      });
+
+      it("sends the request using the PUT method", function() {
+        expect(lastRequest.method).toEqual('PUT');
+      });
+    });
+
+    describe('#isPersisted', function() {
+      it("is true", function() {
+        expect(talk.isPersisted()).toBeTruthy();
+      });
+    });
+  });
+
+  describe('#save', function() {
+    it("sends the talks data in the request body", function() {
+      var talk = new Talk({id: 5, name: "Do the dance" });
+
+      talk.save();
+
+      expect(lastRequest.requestBody).toEqual(JSON.stringify(talk.attributes));
+    });
+
+    it("returns a deferred object");
+
   });
 });
 
