17afcaec7ecda17aa6c035322f0975846b2c9ba7
Zee Spencer <zee@zeespencer.com>
2013-10-20 16:14:55 -0700

    Talks with notes are not listed in upcoming talks

--- collectors/collects_talks_for_rendering.rb
+++ collectors/collects_talks_for_rendering.rb
@@ -1,9 +1,13 @@
 module CollectsTalksForRendering
   def upcoming_talks
-    @upcoming_talks ||= Talk.claimed
+    @upcoming_talks ||= Talk.claimed.select { |t| t.notes.empty? }
   end
 
   def suggested_talks
     @suggested_talks ||= Talk.unclaimed
+  end
+
+  def completed_talks
+    @completed_talks ||= Talk.claimed.reject { |t| t.notes.empty? }
   end
 end
---
+++ spec/features/visiting_home_page_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature "Visiting home page" do
+  context "when not logged in" do
+    scenario "They see a compelling name and description of Thunder" do
+      visit "/"
+
+      expect(page).to have_content "Thunder!"
+
+      expect(page).to have_content "All the Awesome Lightning Talks"
+    end
+  end
+
+  context "When logged in" do
+    scenario "They see a list of upcoming, suggested, and completed talks" do
+      user = create_random_user
+      suggested_talk = create_random_unclaimed_talk
+      claimed_talk = create_random_claimed_talk
+      completed_talk = create_random_completed_talk
+
+      login(user)
+      visit '/'
+
+      within '#suggested-talks' do
+        expect(page).to have_content suggested_talk.name
+      end
+
+      within '#upcoming-talks' do
+        expect(page).to have_content claimed_talk.name
+      end
+
+      within '#completed-talks' do
+        expect(page).to have_content completed_talk.name
+      end
+    end
+  end
+end
