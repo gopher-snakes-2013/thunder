c8f01650970df96e12a71faf933215d7f46661bf
Zee Spencer <zee@zeespencer.com>
2013-10-21 14:34:21 -0700

    Fix tests broken by layout change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,10 +50,10 @@ def login(user)
     user = attributes
   end
   visit '/'
-  click_on "Log in"
+  click_on "log-in-link"
 
   fill_in :user_email, with: user[:email]
   fill_in :user_password, with: user[:password]
 
-  click_on "Log in"
+  click_on "log-in-button"
 end
